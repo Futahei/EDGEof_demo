6f96194c5117cb02230e3210869644608c19ff97
futahei-surface <smilenanakohei0814@gmail.com>
2018-06-01 16:46:18 +0900

    add other ImageTarget

--- Assets/Script/InputHandler.cs
+++ Assets/Script/InputHandler.cs
@@ -7,13 +7,17 @@ public class InputHandler : MonoBehaviour {
 	void Start () {
 		
 	}
+
 	void Update () {
-		var touchPosition = TouchUtil.GetTouchPosition();
-		if(touchPosition!=Vector3.zero) {
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(touchPosition);
-			if(Physics.Raycast(ray, out hit)) {
-				hit.transform.GetComponent<Renderer>().material.color = Color.red;
+		if(TouchUtil.GetTouch()==TouchInfo.Began) {
+			var touchPosition = TouchUtil.GetTouchPosition();
+			if(touchPosition!=Vector3.zero) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+				if(Physics.Raycast(ray, out hit)) {
+					// hit.transform.GetComponent<Renderer>().material.color = Color.red;
+					Application.OpenURL("https://open.spotify.com/track/5HkOHdVwSVKawkkypYnZaZ?si=PDgpAfkPR0yPoqIG7Eo_XA");
+				}
 			}
 		}
 	}
