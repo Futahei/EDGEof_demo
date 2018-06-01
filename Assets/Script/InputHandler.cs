327f45b0201d58c20035ebc75bd1f4d0414e608c
futahei-surface <smilenanakohei0814@gmail.com>
2018-06-01 17:22:01 +0900

    change URL

--- Assets/Script/InputHandler.cs
+++ Assets/Script/InputHandler.cs
@@ -16,7 +16,7 @@ public class InputHandler : MonoBehaviour {
 				Ray ray = Camera.main.ScreenPointToRay(touchPosition);
 				if(Physics.Raycast(ray, out hit)) {
 					// hit.transform.GetComponent<Renderer>().material.color = Color.red;
-					Application.OpenURL("https://open.spotify.com/track/5HkOHdVwSVKawkkypYnZaZ?si=PDgpAfkPR0yPoqIG7Eo_XA");
+					Application.OpenURL("https://geo.itunes.apple.com/jp/album/reason/583843024?i=583843096&app=itunes");
 				}
 			}
 		}
