f8fd8ae31ef71715bd1bf7f06a57e6f8238f1782
DESKTOP-N7Q5U4L\Futahei <futahei1108@icloud.com>
2018-06-01 13:47:39 +0900

    make Input Test

---
+++ Assets/Script/InputHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	void Update () {
+		var touchPosition = TouchUtil.GetTouchPosition();
+		if(touchPosition!=Vector3.zero) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+			if(Physics.Raycast(ray, out hit)) {
+				hit.transform.GetComponent<Renderer>().material.color = Color.red;
+			}
+		}
+	}
+}
