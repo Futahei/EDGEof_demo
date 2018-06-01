e8473dceb014507d5698455dc6d8a3176f1004f8
futahei-surface <smilenanakohei0814@gmail.com>
2018-06-01 17:22:22 +0900

    add texture for URLs

--- Assets/Script/InputHandler.cs
+++ Assets/Script/InputHandler.cs
@@ -16,7 +16,10 @@ public class InputHandler : MonoBehaviour {
 				Ray ray = Camera.main.ScreenPointToRay(touchPosition);
 				if(Physics.Raycast(ray, out hit)) {
 					// hit.transform.GetComponent<Renderer>().material.color = Color.red;
-					Application.OpenURL("https://geo.itunes.apple.com/jp/album/reason/583843024?i=583843096&app=itunes");
+					switch(hit.transform.name) {
+						case "Spotify": Application.OpenURL("https://open.spotify.com/track/5HkOHdVwSVKawkkypYnZaZ?si=PDgpAfkPR0yPoqIG7Eo_XA");break;
+						case "iTunes": Application.OpenURL("https://geo.itunes.apple.com/jp/album/reason/583843024?i=583843096&app=itunes");break;
+					}
 				}
 			}
 		}
